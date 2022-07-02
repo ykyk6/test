9484c130824fcf020eee669eb9986cf57597d4b8
ykyk6 <70152012+ykyk6@users.noreply.github.com>
2022-07-02 18:00:18 +0800

    Delete api.ts

--- src/request/api.ts
+++
@@ -1,9 +0,0 @@
-// import axios from 'axios'
-
-// export const instance = axios.create({
-//   baseURL: process.env.VUE_APP_API
-// })
-
-// instance.interceptors.request.use((config) => {
-//   console.log(config)
-// })
